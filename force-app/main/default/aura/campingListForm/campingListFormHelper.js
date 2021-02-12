ead326813dc9f7d5a21d843c0426763cf153f37c
Sebastián <toloxseba@gmail.com>
2021-02-12 14:19:47 +0100

    .

--- force-app/main/default/aura/campingListForm/campingListFormHelper.js
+++
@@ -1,7 +0,0 @@
-({
-    createItem: function(component, newItem) {
-        let createEvent = component.getEvent("addItem");
-        createEvent.setParams({ "item": newItem });
-        createEvent.fire();
-    },
-})
--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++
@@ -1,8 +0,0 @@
-({
-    packItem:function(component, event, helper) {
-        let item = component.get("v.item");
-        let updateItem = component.getEvent("updateItem");
-        updateItem.setParams({ "item": item });
-        updateItem.fire();
-    }
-})
